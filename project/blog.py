16c5b57345abd2787048bc290871fd66a3ab267e
derfacn <nikitalupalo145@gmail.com>
2023-05-04 15:46:41 +0200

    blog was created

---
+++ project/blog.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, request, url_for, redirect, flash
+from flask_login import login_required, current_user
+from .models import Blog
+from . import db
+from datetime import datetime
+
+blog = Blueprint('blog', __name__)
+
+
+@blog.route('/create')
+@login_required
+def create():
+    return render_template('create.html')
+
+
+@blog.route('/create', methods=['POST'])
+@login_required
+def create_blog():
+    title = request.form.get('title')
+    content = request.form.get('content')
+
+    exists = Blog.query.filter_by(title=title, content=content).first()
+
+    if exists:
+        flash("Post copied! Can't create two posts like this")
+        return redirect(url_for('blog.create'))
+
+    new_blog = Blog(title=title, content=content, date_created=datetime.utcnow(), email=current_user.email,
+                    name=current_user.name)
+
+    db.session.add(new_blog)
+    db.session.commit()
+
+    return redirect(url_for('main.index'))
--- project/models.py
+++ project/models.py
@@ -1,9 +1,19 @@
 from . import db
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(10000), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+    email = db.Column(db.String(100))
+    name = db.Column(db.String(100))
